527da550db033c8e1800b39f248fef991cf66488
MOHAMMED RAFHATH <danarafha@gmail.com>
2023-06-11 22:52:44 +0530

    basic started with some file like signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Netflix from "./pages/Netflix";
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/" element={<Netflix />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import BackgroundImage from "../components/BackgroundImage";
+import Header from "../components/Header";
+export default function Signup() {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <Container showPassword={showPassword}>
+      <BackgroundImage />
+      <div className="content">
+        <Header login />
+        <div className="body flex column a-center j-center">
+          <div className="text flex column">
+            <h1>Unlimited movies, TV shows and more</h1>
+            <h4>Watch anywhere, Cancel anytime. </h4>
+            <h6>
+              Ready to watch ? Enter your email to create or restart membership
+            </h6>
+          </div>
+          <div className="form">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              id="email"
+            />
+            {showPassword && (
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                id="password"
+              />
+            )}
+            {!showPassword && (
+              <button onClick={() => setShowPassword(true)}>Get Started</button>
+            )}
+          </div>
+          <button>Log In</button>
+        </div>
+      </div>
+    </Container>
+  );
+}
+const Container = styled.div`
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+    .body {
+      gap: 1rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        h1 {
+          padding: 0 25rem;
+        }
+      }
+      .form {
+        display: grid;
+        grid-template-columns: ${({ showPassword }) =>
+          showPassword ? "1fr 1fr" : "2fr 1fr"};
+        width: 60%;
+        input {
+          color: black;
+          border: none;
+          padding: 1.5rem;
+          font-size: 1.2rem;
+          border: 1px solid black;
+          &:focus {
+            outline: none;
+          }
+        }
+        button {
+          padding: 0.5rem 1rem;
+          background-color: #e50914;
+          border: none;
+          cursor: pointer;
+          color: white;
+          font-weight: bolder;
+          font-size: 1.05rem;
+        }
+      }
+
+      button {
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        border-radius: 0.2rem;
+        cursor: pointer;
+        color: white;
+        font-weight: bolder;
+        font-size: 1.05rem;
+      }
+    }
+  }
+`;
